04cb91daac4d323473a04ce29150fef51c73a750
naikpriti <91533691+naikpriti@users.noreply.github.com>
2022-07-05 19:51:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-/*variable "clustername" {
+variable "clustername" {
     type = string
     description = "AKS cluster name "
-}*/
+}
 
 variable "location" {
     type = string
@@ -34,8 +34,30 @@ variable "port" {
     description = "port number for the MySQL database"
 }
 */
-/*variable "runbooks_file" {
+variable "runbook_file" {
     type = string
-    
-}*/
+    description = "Azure region where the resources would be provisioned"
+    default = "runbooks.json"
+}
+variable "subscription_id" {
+    type = string
+    description = "Azure region where the resources would be provisioned"
+}
 
+variable "timezone"{
+    type = any
+    description = "resource group name "
+
+}
+
+variable "starttime"{
+    type = string
+    description = "resource group name "
+
+}
+
+variable "stoptime"{
+    type = string
+    description = "resource group name "
+
+}
